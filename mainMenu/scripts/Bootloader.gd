34973489d2c4f52483be4351b6bbf15475d2d252
Drac <gpt.sahaj28@gmail.com>
2023-03-16 17:14:26 +0530

    Added bootloader

---
+++ mainMenu/scripts/Bootloader.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+@onready var logo = $CenterContainer/Logo
+@onready var banner = $CenterContainer/Banner
+@onready var ap = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	logo.visible = true
+	ap.play("fade")
+	await get_tree().create_timer(2.0).timeout
+	ap.play_backwards("fade")
+	await ap.animation_finished
+	logo.visible = false
+	banner.visible = true
+	ap.play("fade")
+	await get_tree().create_timer(3.0).timeout
+	ap.play_backwards("fade")
+	await ap.animation_finished
