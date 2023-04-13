5e806831f0633fec9ca52abc0e1b97dce20eed70
Drac <gpt.sahaj28@gmail.com>
2023-04-14 05:02:57 +0530

    Added Character Class

--- mainMenu/scripts/warning_screen.gd
+++ mainMenu/scripts/warning_screen.gd
@@ -2,9 +2,9 @@ extends Control
 
 @onready var quit_button = $PanelContainer/CenterContainer/VBoxContainer/Panel/QuitButton
 @onready var play_button = $PanelContainer/CenterContainer/VBoxContainer/Panel/PlayButton
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	quit_button.pressed.connect(func(): Utils.quit_game())
+
 
 
