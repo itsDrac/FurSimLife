a679c6b1653bd985d9ea75743d2d43e05c26c7cf
Drac <gpt.sahaj28@gmail.com>
2023-04-14 20:46:07 +0530

    Ready to export Character

--- mainMenu/scripts/warning_screen.gd
+++ mainMenu/scripts/warning_screen.gd
@@ -3,10 +3,13 @@ extends Control
 @onready var quit_button = $PanelContainer/CenterContainer/VBoxContainer/Panel/QuitButton
 @onready var play_button = $PanelContainer/CenterContainer/VBoxContainer/Panel/PlayButton
 @export var player_scene: PackedScene
+
+@onready var cross = $PanelContainer/CenterContainer/VBoxContainer/Label/TextureButton
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	quit_button.pressed.connect(func(): Utils.quit_game())
 	play_button.pressed.connect(func(): Utils.change_scene(get_parent_control(), player_scene.instantiate()))
-
+	cross.pressed.connect(func(): visible = false)
 
 
