950c46405ac638f02b78cfde514cbb4e1d185333
Drac <gpt.sahaj28@gmail.com>
2023-04-14 15:01:18 +0530

    Updated Character and Main menu

--- mainMenu/scripts/warning_screen.gd
+++ mainMenu/scripts/warning_screen.gd
@@ -2,9 +2,11 @@ extends Control
 
 @onready var quit_button = $PanelContainer/CenterContainer/VBoxContainer/Panel/QuitButton
 @onready var play_button = $PanelContainer/CenterContainer/VBoxContainer/Panel/PlayButton
+@export var player_scene: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	quit_button.pressed.connect(func(): Utils.quit_game())
+	play_button.pressed.connect(func(): Utils.change_scene(get_parent_control(), player_scene.instantiate()))
 
 
 
